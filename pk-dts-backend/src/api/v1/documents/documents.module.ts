0bf169cb7906115e803b89f32a89770fb866e0c5
John Paul Cajes Curib <109894757+curib123@users.noreply.github.com>
2026-09-14 20:42:43 +0800

    feat: enable request-time document service

--- pk-dts-backend/src/api/v1/documents/documents.module.ts
+++ pk-dts-backend/src/api/v1/documents/documents.module.ts
@@ -3,9 +3,14 @@ import { DocumentsController } from "./documents.controller";
 import { DocumentsService } from "./documents.service";
 import { PublicDocumentsController } from "./public-documents.controller";
 import { ElectronicDocumentStampService } from "./electronic-document-stamp.service";
+import { RequestTimeDocumentsService } from "./request-time-documents.service";
 
 @Module({
   controllers: [DocumentsController, PublicDocumentsController],
-  providers: [DocumentsService, ElectronicDocumentStampService],
+  providers: [
+    ElectronicDocumentStampService,
+    RequestTimeDocumentsService,
+    { provide: DocumentsService, useExisting: RequestTimeDocumentsService },
+  ],
 })
 export class DocumentsModule {}
